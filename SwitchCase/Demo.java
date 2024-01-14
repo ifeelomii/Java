e43889aaf7514cbad4284260764d47c68f252d7e
Omkar Ware <123940891+ifeelomii@users.noreply.github.com>
2024-01-14 22:00:01 +0530

    Delete SwitchCase directory

--- SwitchCase/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo{
-    public static void main(String[] args) {
-        String s1="Hello From Java";
-        char c1=s1.charAt(0);
-        System.out.println(c1);
-        char chrs[]=s1.toCharArray();
-        System.out.println(chrs[2]);
-    }
-}
